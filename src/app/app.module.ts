8f068dfeba53a076fe25d223514e81decc17b216
laurarestrepo <laura_restrepo_01@hotmail.com>
2017-10-03 15:49:06 -0500

    error de instalacion de angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,110 @@ import { IndexComponent } from './candidato/index/index.component';
 import { AgregarComponent} from'./candidato/agregar/agregar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {  
+  
+  MdAutocompleteModule,
+  
+  
+  MdButtonModule,
+  
+  
+  MdButtonToggleModule,
+  
+  
+  MdCardModule,
+  
+  
+  MdCheckboxModule,
+  
+  
+  MdChipsModule,
+  
+  
+  MdCoreModule,
+  
+  
+  MdDatepickerModule,
+  
+  
+  MdDialogModule,
+  
+  
+  MdExpansionModule,
+  
+  
+  MdGridListModule,
+  
+  
+  MdIconModule,
+  
+  
+  MdInputModule,
+  
+  
+  MdListModule,
+  
+  
+  MdMenuModule,
+  
+  
+  MdNativeDateModule,
+  
+  
+  MdPaginatorModule,
+  
+  
+  MdProgressBarModule,
+  
+  
+  MdProgressSpinnerModule,
+  
+  
+  MdRadioModule,
+  
+  
+  MdRippleModule,
+  
+  
+  MdSelectModule,
+  
+  
+  MdSidenavModule,
+  
+  
+  MdSliderModule,
+  
+  
+  MdSlideToggleModule,
+  
+  
+  MdSnackBarModule,
+  
+  
+  MdSortModule,
+  
+  
+  MdTableModule,
+  
+  
+  MdTabsModule,
+  
+  
+  MdToolbarModule,
+  
+  
+  MdTooltipModule,
+  
+  } from
+  '@angular/material';
 import { AppComponent } from './app.component';
 import { CandidatoModule } from './candidato/candidato.module';
 import { MenuComponent } from './menu/menu.component';
 import { CurrentOptionComponent } from './current-option/current-option.component';
+import {BrowserAnimationsModule} from
+'@angular/platform-browser/animations';
+
+  
 
 
 const appRoutes: Routes =[
@@ -24,8 +123,99 @@ const appRoutes: Routes =[
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CandidatoModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes), MdAutocompleteModule,
+    
+    
+    MdButtonModule,
+    
+    
+    MdButtonToggleModule,
+    
+    
+    MdCardModule,
+    
+    
+    MdCheckboxModule,
+    
+    
+    MdChipsModule,
+    
+    
+    MdCoreModule,
+    
+    
+    MdDatepickerModule,
+    
+    
+    MdDialogModule,
+    
+    
+    MdExpansionModule,
+    
+    
+    MdGridListModule,
+    
+    
+    MdIconModule,
+    
+    
+    MdInputModule,
+    
+    
+    MdListModule,
+    
+    
+    MdMenuModule,
+    
+    
+    MdNativeDateModule,
+    
+    
+    MdPaginatorModule,
+    
+    
+    MdProgressBarModule,
+    
+    
+    MdProgressSpinnerModule,
+    
+    
+    MdRadioModule,
+    
+    
+    MdRippleModule,
+    
+    
+    MdSelectModule,
+    
+    
+    MdSidenavModule,
+    
+    
+    MdSliderModule,
+    
+    
+    MdSlideToggleModule,
+    
+    
+    MdSnackBarModule,
+    
+    
+    MdSortModule,
+    
+    
+    MdTableModule,
+    
+    
+    MdTabsModule,
+    
+    
+    MdToolbarModule,
+    
+    
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
